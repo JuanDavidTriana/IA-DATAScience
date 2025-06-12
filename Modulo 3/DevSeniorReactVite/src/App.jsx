d65b3c9ca03a905b162e6a492baebdbd3bebba4a
JuanDavidTrianaT <juandavidtriana.jdt@gmai.com>
2025-06-11 20:05:05 -0500

    Add serialized model file for preprocessing and regression pipeline
    - Introduced a joblib file containing a preprocessor and a linear regression model.
    - The preprocessor includes a column transformer with numerical and categorical transformations.
    - Implemented imputation for missing values and standard scaling for numerical features.
    - Categorical features are encoded using one-hot encoding.

---
+++ Modulo 3/DevSeniorReactVite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main className="h-screen w-full bg-gray-950 flex flex-col justify-center items-center gap-4">
+      <h1 className="text-5xl font-bold text-white">
+        React + Vite + Tailwind CSS
+      </h1>
+      <p className="text-xl text-emerald-400">
+        Instalación con @tailwindcss/vite ¡funcionando!
+      </p>
+    </main>
+  )
+}
+
+export default App
